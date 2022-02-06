2adf5918e3ac02f8b1ee2db5e51bffd279637ebb
Zakhar Gulchak <gulchak.zahar@gmail.com>
2022-02-06 16:34:54 +0200

    fixed format

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   collectCoverage: true,
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js']
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
 }
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,37 +3,37 @@ import HtmlWebPackPlugin from 'html-webpack-plugin'
 
 export default {
   entry: {
-    main: path.resolve(__dirname, 'src/App.tsx')
+    main: path.resolve(__dirname, 'src/App.tsx'),
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].[hash].js'
+    filename: '[name].[hash].js',
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json']
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
   },
   devServer: {
     port: 8080,
-    liveReload: true
+    liveReload: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html'
-    })
-  ]
+      template: './src/index.html',
+    }),
+  ],
 }
